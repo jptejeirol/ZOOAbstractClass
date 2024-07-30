f8ff8d4d505056bf8009c1b09b6b01832280d276
Néstor Bolívar <42524922+NBOLIVARTELECO@users.noreply.github.com>
2024-07-30 07:48:19 -0500

    First Version

---
+++ src/zoologico/Zoo.java
@@ -0,0 +1,18 @@
+
+package zoologico;
+
+/**
+ *
+ * @author Néstor
+ */
+public abstract class Zoo{
+    
+   Animales jaulas[];
+   
+   public abstract double cantar(double num);
+   
+   public int alojar(){
+       return 1;
+   }
+ 
+}
---
+++ src/zoologico/Zoologico.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoologico;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Zoologico {
+
+  
+    public static void main(String[] args) {
+       //Zoo zoo1 = new Zoo();
+       Zoo ZooAndes = new Animales("Cundinamarca");
+       //pajaro.cantar(1500);
+       ZooAndes.jaulas[0] = new Animales("Pajaro");
+        //pajaro.cantar(1500);
+       
+       
+       
+//       zoo1.varios = new Animales[5];
+//       
+//       Animales animal1 = new Animales("Jirafa");
+//       Animales animal2 = new Animales("Elefante");
+//       
+//       zoo1.varios[0] = animal1;
+//       
+//       //Relación por composición\
+//       
+//       ordenPedido orden1 = new ordenPedido();
+       
+      
+  
+    }
+    
+}
